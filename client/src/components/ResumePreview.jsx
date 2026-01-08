349339653f1a56bacb43159ae359acf4ac0cd6b5
Shanuka Wanniarachchi <shanukawanniarachchi2000@gmail.com>
2026-01-09 00:01:28 +0530

    templtes

---
+++ client/src/components/ResumePreview.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ResumePreview = () => {
+  return (
+    <div className='w-full bg-gray-100'>
+        <div id="resume-preview" className={"border border-gray-200 print:shadow-none print:border-none" + classes}>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default ResumePreview
