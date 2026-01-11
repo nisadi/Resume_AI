224e563eb3a2fc4bbbacd85f97e9be30a1d923e0
Shanuka Wanniarachchi <shanukawanniarachchi2000@gmail.com>
2026-01-12 00:52:28 +0530

    skillForm&bugClear

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,7 @@ import ProfessionalSummary from "../components/ProfessionalSummary";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 
 
 const ResumeBuilder = () => {
@@ -200,7 +201,22 @@ const ResumeBuilder = () => {
                       }
                     />
                   )}
+                  {activeSection.id === "skills" && (
+                    <SkillsForm
+                      data={resumeData.skills}
+                      onChange={(data) =>
+                        setResumeData((prev) => ({
+                          ...prev,
+                          skills: data,
+                        }))
+                      }
+                    />
+                  )}
                 </div>
+                <button className="bg-gradient-to-br from-green-100 to-green-200 ring-green-300 text-green-600 ring hover:ring-green-400 
+                transition-all rounded-md px-6 py-2 mt-6 text-sm">
+                  Save Changes
+                </button>
               </div>
             </div>
 
