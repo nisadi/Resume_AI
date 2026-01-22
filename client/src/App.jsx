63c9136d016b449a2cb31aa8474f688698311526
umeshwickramaarachchi <umeshmihiranga24@gmail.com>
2026-01-22 11:07:49 +0530

    comit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
+import React from "react";
+import Login from "./pages/Login";
 
 function App() {
-  
-
   return (
     <>
-    <h1>Home</h1>
+      <Login />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
